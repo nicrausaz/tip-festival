ba112568640e87556d6ff58fcafe58d720c271de
nicrausaz <n.crausaz99@gmail.com>
2018-11-20 13:53:40 +0100

    infos

--- src/pages/Infos.vue
+++ src/pages/Infos.vue
@@ -8,29 +8,50 @@
         </q-card-title>
       </q-card-media>
       <q-card-main>
-        Nous vous recommendons les transports en commun
+        <p><b>Nous vous recommandons les transports en commun</b></p>
+        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Accès aux bus :</p>
+        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
+        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p>Plus d'informations sur <a href="https://www.t-l.ch/">https://www.t-l.ch/</a></p>
       </q-card-main>
     </q-card>
     <q-card inline class="q-ma-sm sectionsItem">
       <q-card-media>
-        <img src="statics/planaccess.png">
+        <img src="statics/parking.jpg">
         <q-card-title slot="overlay">
-          Accès
+          Parking
         </q-card-title>
       </q-card-media>
       <q-card-main>
-        L'accès au festival se fait ...
+        <p><b>Nous vous recommandons les transports en commun</b></p>
+        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Accès aux bus :</p>
+        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
+        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p>Plus d'informations sur <a href="https://www.t-l.ch/">https://www.t-l.ch/</a></p>
       </q-card-main>
     </q-card>
     <q-card inline class="q-ma-sm sectionsItem">
       <q-card-media>
-        <img src="statics/metro.jpeg">
+        <img src="statics/eatndrink.jpg">
         <q-card-title slot="overlay">
           Restauration
         </q-card-title>
       </q-card-media>
       <q-card-main>
-        Nous vous recommendons les transports en commun
+        <p><b>Nous vous recommandons les transports en commun</b></p>
+        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Accès aux bus :</p>
+        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
+        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p>Plus d'informations sur <a href="https://www.t-l.ch/">https://www.t-l.ch/</a></p>
       </q-card-main>
     </q-card>
     <q-card inline class="q-ma-sm sectionsItem">
@@ -41,7 +62,14 @@
         </q-card-title>
       </q-card-media>
       <q-card-main>
-        L'entrée au festival est libre !
+        <p><b>Nous vous recommandons les transports en commun</b></p>
+        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
+        <p>Accès aux bus :</p>
+        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
+        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
+        <p>Plus d'informations sur <a href="https://www.t-l.ch/">https://www.t-l.ch/</a></p>
       </q-card-main>
     </q-card>
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
@@ -60,5 +88,8 @@ export default {
 </script>
 
 <style scoped>
-
+.sectionsItem {
+  width: 800px;
+  /* height: 800px; */
+}
 </style>
