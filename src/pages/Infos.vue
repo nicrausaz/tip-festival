a62fd20ca1fac1125f5b1dc1311dc6a2651eee81
nicrausaz <n.crausaz99@gmail.com>
2019-01-13 15:08:54 +0100

    infos

--- src/pages/Infos.vue
+++ src/pages/Infos.vue
@@ -13,24 +13,6 @@
     </q-card>
     <q-card inline class="q-ma-sm sectionsItem">
       <q-card-media>
-        <img src="statics/parking.jpg">
-        <q-card-title slot="overlay">
-          {{$store.state.moduleLanguage.translations.festival_access}}
-        </q-card-title>
-      </q-card-media>
-      <q-card-main>
-        <p><b>Nous vous recommandons les transports en commun</b></p>
-        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
-        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
-        <p>Accès aux bus :</p>
-        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
-        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
-        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
-        <p>Plus d'informations sur <a href="https://www.t-l.ch/">https://www.t-l.ch/</a></p>
-      </q-card-main>
-    </q-card>
-    <q-card inline class="q-ma-sm sectionsItem">
-      <q-card-media>
         <img src="statics/eatndrink.jpg">
         <q-card-title slot="overlay">
           Restauration
@@ -40,40 +22,19 @@
         <p v-html="$store.state.moduleLanguage.infosTexts.food"></p>
       </q-card-main>
     </q-card>
-    <q-card inline class="q-ma-sm sectionsItem">
-      <q-card-media>
-        <img src="statics/metro.jpeg">
-        <q-card-title slot="overlay">
-          Tarifs
-        </q-card-title>
-      </q-card-media>
-      <q-card-main>
-        <p><b>Nous vous recommandons les transports en commun</b></p>
-        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
-        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
-        <p>Accès aux bus :</p>
-        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
-        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
-        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
-        <p>Plus d'informations sur <a href="https://www.t-l.ch/" target="_blank">https://www.t-l.ch/</a></p>
-      </q-card-main>
-    </q-card>
      <q-card inline class="q-ma-sm sectionsItem">
       <q-card-media>
-        <img src="statics/metro.jpeg">
+        <img src="statics/guitar.jpg">
         <q-card-title slot="overlay">
           Réglement
         </q-card-title>
       </q-card-media>
       <q-card-main>
-        <p><b>Nous vous recommandons les transports en commun</b></p>
-        <p>Bus 1 et 6 arrêt Maladière, Lausanne (dernier départ bus 1 à env. 00:15, dernier départ bus 6 à env. 00:03, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
-        <p>Bus 2 arrêt Théâtre de Vidy (dernier départ à env. 00:16, horaire selon le vendredi 05.08.2018 et le samedi 06.08.2018, il est possible que les horaires changent d’ici le vendredi 5 juillet).</p>
-        <p>Accès aux bus :</p>
-        <p><b>Ligne 1</b>: Depuis la gare de Lausanne, prendre le bus 1 jusqu’à l’arrêt Maladière, Lausanne.</p>
-        <p><b>Ligne 2</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à Ouchy Olympique (dernier métro à env. 00:30) puis prendre le bus 2 jusqu’à l’arrêt Théâtre de Vidy.</p>
-        <p><b>Ligne 6</b>: Depuis la gare de Lausanne, prendre le M2 jusqu’à l’arrêt Lausanne-Flon (dernier métro à env. 00:30), puis marcher jusqu’à l’arrêt Bel-Air pour y prendre le bus 6 jusqu’à l’arrêt Maladière, Lausanne.</p>
-        <p>Plus d'informations sur <a href="https://www.t-l.ch/" target="_blank">https://www.t-l.ch/</a></p>
+        <!--<p v-html="$store.state.moduleLanguage.infosTexts.food"></p>-->
+        <p><b>Merci de tenir compte des indications suivantes !</b></p>
+        <p>Le festival Fest'HipHop se déroule aux Pyramides de Vidy à Lausanne.</p>
+        <p>Ce festival est libre d'accès et se déroule dans un espace public, nous vous demandons de bien vouloir respecter
+        le bien-être et la sécurité d'autrui dans l'enceinte du festival, adoptez un comportement fair-play et responsable !</p>
       </q-card-main>
     </q-card>
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
@@ -97,6 +58,5 @@ export default {
 <style scoped>
 .sectionsItem {
   width: 800px;
-  /* height: 800px; */
 }
 </style>
