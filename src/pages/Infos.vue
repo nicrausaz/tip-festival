9f56e988cf7d99b9a6d2a785b86ae0055012e47e
nicrausaz <n.crausaz99@gmail.com>
2019-01-14 15:20:20 +0100

    infos

--- src/pages/Infos.vue
+++ src/pages/Infos.vue
@@ -13,9 +13,24 @@
     </q-card>
     <q-card inline class="q-ma-sm sectionsItem">
       <q-card-media>
+        <img src="statics/parking.jpg">
+        <q-card-title slot="overlay">
+          {{$store.state.moduleLanguage.translations.festival_parking}}
+        </q-card-title>
+      </q-card-media>
+      <q-card-main>
+        <p><b>Un paking public est à disposition</b></p>
+        <p>Cependant, le nombre de places est restreint et le parking est payant.</p>
+        <p>Nous vous conseillons donc l'usage des transports en commun.</p>
+        <p><a target="_blank" href="https://www.google.com/maps/place/Parking+de+Vidy/@46.5135965,6.6123016,3a,75y,225.49h,90t/data=!3m7!1e1!3m5!1s0cn-OC4OBsnA9gcK2_q-2g!2e0!6s%2F%2Fgeo1.ggpht.com%2Fcbk%3Fpanoid%3D0cn-OC4OBsnA9gcK2_q-2g%26output%3Dthumbnail%26cb_client%3Dsearch.TACTILE.gps%26thumb%3D2%26w%3D86%26h%3D86%26yaw%3D225.49373%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656!4m8!1m2!2m1!1sParking+de+Vidy!3m4!1s0x0:0xa9a40112da4bf393!8m2!3d46.5133974!4d6.6120207">Localisation du parking</a></p>
+        <p v-html="$store.state.moduleLanguage.infosTexts.parking"></p>
+      </q-card-main>
+    </q-card>
+    <q-card inline class="q-ma-sm sectionsItem">
+      <q-card-media>
         <img src="statics/eatndrink.jpg">
         <q-card-title slot="overlay">
-          Restauration
+          {{$store.state.moduleLanguage.translations.festival_restoration}}
         </q-card-title>
       </q-card-media>
       <q-card-main>
@@ -26,15 +41,11 @@
       <q-card-media>
         <img src="statics/guitar.jpg">
         <q-card-title slot="overlay">
-          Réglement
+          {{$store.state.moduleLanguage.translations.festival_rules}}
         </q-card-title>
       </q-card-media>
       <q-card-main>
-        <!--<p v-html="$store.state.moduleLanguage.infosTexts.food"></p>-->
-        <p><b>Merci de tenir compte des indications suivantes !</b></p>
-        <p>Le festival Fest'HipHop se déroule aux Pyramides de Vidy à Lausanne.</p>
-        <p>Ce festival est libre d'accès et se déroule dans un espace public, nous vous demandons de bien vouloir respecter
-        le bien-être et la sécurité d'autrui dans l'enceinte du festival, adoptez un comportement fair-play et responsable !</p>
+        <p v-html="$store.state.moduleLanguage.infosTexts.rules"></p>
       </q-card-main>
     </q-card>
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
