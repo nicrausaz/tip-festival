44c40f111e69a578415d442f040dfb03b7bee296
nicrausaz <n.crausaz99@gmail.com>
2019-01-15 15:07:38 +0100

    total order

--- src/store/module-order/getters.js
+++ src/store/module-order/getters.js
@@ -1,8 +1,7 @@
 export function getTotalPrice (state) {
   let total = 0
   state.order_infos_content.articles.forEach(item => {
-    // TODO: * quantity
-    total += item.price
+    total += item.price * item.quantity
   })
   // 5 shipping
   return total + 5
